68e5076496d56761022b483c0d37788180020d38
B1ueSky <programmer.lty@gmail.com>
2014-03-06 22:52:01 -0500

    try to travel stack in my_printf.c

--- my_printf.c
+++ my_printf.c
@@ -7,11 +7,12 @@
 #include "itoa.c"
 #include <stdio.h>
 
-void my_printf(const char *, char *);
+void my_printf(const char *, ...);
 
 void
-my_printf(const char * format, char *s)
+my_printf(const char * format, ...)
 {
+    /*
     char * ptr = format;
     
     while ((*ptr) != 0)
@@ -26,19 +27,21 @@ my_printf(const char * format, char *s)
         printf("%x\n", ptr);
         printf("%d\n", *ptr++);
     }
+    */
     /* get the first ar */
+    /*
     //ptr++;
     while((*ptr) != 0)
     {
         printf("%x\n", ptr);
         printf("%d\n", *ptr);
         printf("%c\n", *ptr++);
-    }
+    }*/
 }
 
 int 
 main(int argc, char * argv[])
 {
-    my_printf("asdf", "jkl;");
+    my_printf("asdf", "jkl;", 123);
     return 0;
 }
