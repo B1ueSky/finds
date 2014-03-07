ea2fab0a5fd06c67cb9e780410e0121d4f95a4eb
B1ueSky <programmer.lty@gmail.com>
2014-03-06 19:47:19 -0500

    try to traverse the stack manually in my_printf.c file.

---
+++ my_printf.c
@@ -0,0 +1,44 @@
+/* File:	my_printf.c
+ * Name:	Tianyou Luo
+ * Date:	03/17/14
+ * Purpose: same function of printf() in standard I/O libary
+ */
+
+#include "itoa.c"
+#include <stdio.h>
+
+void my_printf(const char *, char *);
+
+void
+my_printf(const char * format, char *s)
+{
+    char * ptr = format;
+    
+    while ((*ptr) != 0)
+    {
+        printf("%x\n", ptr);
+        printf("%s\n", ptr++);
+        
+    }
+    printf("%s\n", *++ptr);
+    while (*ptr == 0)
+    {
+        printf("%x\n", ptr);
+        printf("%d\n", *ptr++);
+    }
+    /* get the first ar */
+    //ptr++;
+    while((*ptr) != 0)
+    {
+        printf("%x\n", ptr);
+        printf("%d\n", *ptr);
+        printf("%c\n", *ptr++);
+    }
+}
+
+int 
+main(int argc, char * argv[])
+{
+    my_printf("asdf", "jkl;");
+    return 0;
+}
