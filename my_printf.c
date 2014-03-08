3d3684ab97b5fc46c9af82f3db88a18dce4fd3e6
B1ueSky <programmer.lty@gmail.com>
2014-03-07 21:56:26 -0500

    figure out how to travel stack.

--- my_printf.c
+++ my_printf.c
@@ -6,42 +6,34 @@
 
 #include "itoa.c"
 #include <stdio.h>
-
+#include <stdlib.h>
 void my_printf(const char *, ...);
+
+int getesp() {
+__asm__("movl %esp,%eax");
+}
+
+int getebp()
+{
+__asm__("movl %ebp,%eax");
+}
 
 void
 my_printf(const char * format, ...)
 {
-    /*
-    char * ptr = format;
+    int i = 0;
+
+    printf("%s\n", *(&format+1));
+    void ** arg_ptr;
+    arg_ptr  = &format +1;
     
-    while ((*ptr) != 0)
-    {
-        printf("%x\n", ptr);
-        printf("%s\n", ptr++);
-        
-    }
-    printf("%s\n", *++ptr);
-    while (*ptr == 0)
-    {
-        printf("%x\n", ptr);
-        printf("%d\n", *ptr++);
-    }
-    */
-    /* get the first ar */
-    /*
-    //ptr++;
-    while((*ptr) != 0)
-    {
-        printf("%x\n", ptr);
-        printf("%d\n", *ptr);
-        printf("%c\n", *ptr++);
-    }*/
+    printf("%x\n", arg_ptr);
+    printf("%s\n", (char*)(arg_ptr));
 }
 
 int 
 main(int argc, char * argv[])
 {
-    my_printf("asdf", "jkl;", 123);
+    my_printf("asdfasdfasdf", "jkl",123,"werwerwrwerwer",233);
     return 0;
 }
